e936c10e353187f132f6fac54cfe2eac3afc4839
Briitely <info@briitely.com>
2026-02-18 13:02:27 -0700

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ const NAVIGATE_LINKS = [
 function MapleLeaf() {
   return (
     <svg
-      className="w-7 h-7 flex-shrink-0"
+      className="w-6 h-6 flex-shrink-0"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 375 375"
       aria-hidden="true"
