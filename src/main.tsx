ffb4e6f380e9b4395986cf06585bd66c2985f013
Lana Walsh <info@briitely.com>
2026-02-24 17:58:32 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App.tsx';
 import './index.css';
 import 'leaflet/dist/leaflet.css';
 
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </StrictMode>
+);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
